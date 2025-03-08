5105c2566bd1da24d28b0e8538ea6d0469a6ce51
lucianorb19 <52081214+lucianorb19@users.noreply.github.com>
2025-03-08 12:43:23 -0300

    Update - Data Output

--- PrimeiroProjeto/PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -2,6 +2,8 @@
 //Console.WriteLine("Hello, World!");
 
 using System;//REFERÊNCIA AO NAMESPACE SYSTEM - PADRÃO DO VISUAL STUDIO
+using System.Globalization; //CultureInfo.InvariantCulture
+
 
 namespace PrimeiroProjeto
 {
@@ -9,10 +11,25 @@ namespace PrimeiroProjeto
     {
         static void Main(string[] args)//ENTRY POINT - ONDE A APLICÃÇÃO COMEÇA A SER EXECUTADA
         {
-            object x = 45.2f;
-            float y = 50.5f;
-            Console.WriteLine(x.GetType());
-            Console.WriteLine(y.GetType());
+            //EXERCÍCIO_1
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritório";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+
+            Console.WriteLine($"{produto1} cujo preço é ${preco1.ToString("f2")}");
+            Console.WriteLine($"{produto2}, cujo preço é ${preco2.ToString("f2")}");
+            Console.WriteLine($"Registo: {idade} anos de idade, código {codigo} e gênero: {genero}");
+            Console.WriteLine($"Medida com oito casas decimais: {medida.ToString("f8")}");
+            Console.WriteLine($"Arredondado (três casas decimais): {medida.ToString("f3")}");
+            Console.WriteLine($"Separador decimal invariant culture: {medida.ToString("f3", CultureInfo.InvariantCulture)}");
+
 
         }
     }
