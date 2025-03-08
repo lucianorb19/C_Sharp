fcf9f58977a3a610b13099c10c414aa195a41da9
lucianorb19 <52081214+lucianorb19@users.noreply.github.com>
2025-03-08 11:29:35 -0300

    Introduction
    C# variables types

---
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;//REFERÊNCIA AO NAMESPACE SYSTEM - PADRÃO DO VISUAL STUDIO
+
+namespace PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)//ENTRY POINT - ONDE A APLICÃÇÃO COMEÇA A SER EXECUTADA
+        {
+            object x = 45.2f;
+            float y = 50.5f;
+            Console.WriteLine(x.GetType());
+            Console.WriteLine(y.GetType());
+
+        }
+    }
+}
