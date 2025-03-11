deec2d29f329311a14bfd35c288697a18721f4f5
lucianorb19 <52081214+lucianorb19@users.noreply.github.com>
2025-03-11 12:21:25 -0300

    Object Oriented Programing - introduction

---
+++ PrimeiroProjetoOrientadoObjeto/PrimeiroProjetoOrientadoObjeto/Program.cs
@@ -0,0 +1,54 @@
+﻿using PrimeiroProjetoOrientadoObjeto;
+using System;
+using System.Globalization;
+
+
+namespace PrimerioProjetoOrientadoObjeto
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ÁREA DE TRIÂNGULO USANDO CLASSE TRIANGULO E FÓRMULAR DE HERON
+
+            Triangulo x, y; //INICIALIZAÇÃO DOS OBJETOS TRIÂNGULO
+
+            x = new Triangulo();
+            y = new Triangulo();//INSTANCIAÇÃO DOS OBJETOS TRIANGULO
+
+            Console.WriteLine("Lados primeiro triangulo: ");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Lados segundo triangulo: ");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double p = (x.A + x.B + x.C) / 2;
+            double area_x = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+
+            p = (y.A + y.B + y.C) / 2;
+            double area_y = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+
+            Console.WriteLine($"Área do triangulo X: {area_x.ToString("f4", CultureInfo.InvariantCulture)}\n" +
+                                  $"Área do triângulo Y: {area_y.ToString("f4", CultureInfo.InvariantCulture)}");
+
+            if (area_x == area_y)
+            {
+                Console.WriteLine("Áreas iguais!");
+            }
+            else if (area_x > area_y)
+            {
+                Console.WriteLine("Maior área: triângulo X");
+            }
+            else // areay > areax
+            {
+                Console.WriteLine("Maior área: triângulo Y");
+            }
+
+        }
+    }
+}
--- PrimeiroProjetoOrientadoObjeto/PrimeiroProjetoOrientadoObjeto/obj/Debug/net8.0/PrimeiroProjetoOrientadoObjeto.AssemblyInfo.cs
+++ PrimeiroProjetoOrientadoObjeto/PrimeiroProjetoOrientadoObjeto/obj/Debug/net8.0/PrimeiroProjetoOrientadoObjeto.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("PrimeiroProjeto")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("PrimeiroProjetoOrientadoObjeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8a740991d5377b5303ab82ad83d84494a636cc9")]
-[assembly: System.Reflection.AssemblyProductAttribute("PrimeiroProjeto")]
-[assembly: System.Reflection.AssemblyTitleAttribute("PrimeiroProjeto")]
+[assembly: System.Reflection.AssemblyProductAttribute("PrimeiroProjetoOrientadoObjeto")]
+[assembly: System.Reflection.AssemblyTitleAttribute("PrimeiroProjetoOrientadoObjeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
