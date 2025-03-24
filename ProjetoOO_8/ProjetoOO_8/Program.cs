5399580bfe01023f67a42dadee6b228999b5af3b
lucianorb19 <52081214+lucianorb19@users.noreply.github.com>
2025-03-24 10:46:05 -0300

    Update Notes - Try Catch, finally, DomainExceptions

---
+++ ProjetoOO_8/ProjetoOO_8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ProjetoOO_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Check In: [DD/MM/YYYY]");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Check Out: [DD/MM/YYYY]");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+            TimeSpan duration = checkOut.Subtract(checkIn);
+            int result = (int)duration.TotalDays;
+            Console.WriteLine(result);
+            */
+            
+        }
+    }
+}
