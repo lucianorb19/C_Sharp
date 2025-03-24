12c9c5f5bc3a3db829f1b47be62297c6ba07b0f9
lucianorb19 <52081214+lucianorb19@users.noreply.github.com>
2025-03-24 14:35:53 -0300

    File Operations
    File, FileInfo, FileStream, StreamReader and StreamWriter

---
+++ ProjetoOO_9/ProjetoOO_9/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO; //FILES
+namespace ProjetoOO_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CAMINHO ARQUIVO ORIGEM
+            string sourcePath = @"C:\Users\CREAS\Desktop\arquivos\file1.txt";
+            string targetPath = @"C:\Users\CREAS\Desktop\arquivos\file2.txt";
+
+            try
+            {
+                //VETOR EM QUE CADA ITEM É UMA LINHA DO ARQUIVO ORIGINAL
+                string[] lines = File.ReadAllLines(sourcePath);
+                //MODO DE ESCRITA - NO FINAL DO ARQUIVO
+                using(StreamWriter sw = File.AppendText(targetPath))
+                {
+                    //CADA ITEM DO VETOR É ESCRITO NO FINAL DO ARQUIVO - EM MAIÚSCULO
+                    //ESCREVE UMA LINHA - ENTER
+                    foreach(string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"Erro. {e.Message}");
+            }
+
+
+        }
+    }
+}
