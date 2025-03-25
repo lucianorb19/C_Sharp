6a8471b46583b4fb6ff74ec907252024fcd6356c
lucianorb19 <52081214+lucianorb19@users.noreply.github.com>
2025-03-25 16:58:13 -0300

    Generics

---
+++ ProjetoOO_11/ProjetoOO_11/PrintService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoOO_11
+{
+    class PrintService<T>
+    {
+        private T[] _values = new T[10];
+        private int _count = 0;
+        
+        //CONSTRUTORES
+
+        
+        //DEMAIS MÉTODOS
+        //MÉTODO QUE ADICIONA UM VALOR AO VETOR
+        public void AddValue(T value)
+        {
+            if(_count == 10)//
+            {
+                throw new InvalidOperationException("PrintService Full");
+            }
+
+            _values[_count] = value;
+            _count++;
+        }
+
+        //MÉTODO QUE RETORNA O PRIMEIRO ITEM DO VETOR
+        public T First()
+        {
+            return _values[0];
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("[ ");
+            for(int i = 0; i < _values.Length; i++)
+            {
+                if(i <= _count-1)//ATÉ O PENÚLTIMO
+                {
+                    sb.Append($"{_values[i]},");
+                }
+                else
+                {
+                    sb.Append($"{_values[i]}");
+
+                }
+            }
+            sb.Append(" ]");
+
+            return sb.ToString();
+        }
+
+
+    }
+}
---
+++ ProjetoOO_11/ProjetoOO_11/Program.cs
@@ -0,0 +1,33 @@
+﻿using ProjetoOO_11;
+using System;
+
+namespace ProjetoOO_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //NO PROGRAMA PRINCIPAL, DEFINIR QUAL TIPO SERÁ USADO NO LUGAR NO GENERIC T
+            PrintService<string> printService = new PrintService<string>();
+
+            Console.Write("How many values: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++)
+            {
+                string x = Console.ReadLine();
+                printService.AddValue(x);
+            }
+
+            Console.WriteLine(printService);
+
+            string a = printService.First();
+            Console.WriteLine($"First: {a}");
+
+
+
+
+        }
+    }
+}
