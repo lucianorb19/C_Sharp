eaba02b7a137b6ebc341d35b0fb898881889f22e
lucianorb19 <52081214+lucianorb19@users.noreply.github.com>
2025-03-24 22:49:02 -0300

    Exercise - File - folder

--- ProjetoOO_9/ProjetoOO_9/Program.cs
+++ ProjetoOO_9/ProjetoOO_9/Program.cs
@@ -9,36 +9,40 @@ namespace ProjetoOO_9
         static void Main(string[] args)
         {
             //PASTA ARQUIVO ORIGINAL
-            string sourcePath = @"C:\Users\CREAS\Desktop\myfolder\file1.txt";
+            string sourcePath = @"C:\Users\Luciano\Desktop\myfolder\file1.txt";
 
             try
             {
                 //CRIANDO PASTA PARA ARQUIVO SAÍDA
                 string targetPath = Path.GetDirectoryName(sourcePath) + @"\out";
                 Directory.CreateDirectory(targetPath);
+                //TORNANDO A MESMA VARIÁVEL O PRÓPRIO ARQUIVO - PARA ESCRITA
+                targetPath = targetPath + @"\summary.txt";
 
-
+                //FileStream - ABRE OU CRIA, CASO AINDA NÃO HAJA
                 using (FileStream fs = new FileStream(sourcePath, FileMode.OpenOrCreate))
                 {
                     using (StreamReader sr = new StreamReader(fs))
                     {
+                        //ENQUANTO NÃO CHEGAR AO FINAL DO ARQUIVO
                         while (!sr.EndOfStream)
                         {
-                            string linha = sr.ReadLine();
-                            string[] dados_linha = linha.Split(",");
+                            string linha = sr.ReadLine();//SALVO CADA LINHA EM linha
+                            string[] dados_linha = linha.Split(",");//DIVIDO A LINHA EM ELEMENTOS - SEPARADOR: ,
                             string nome = dados_linha[0];
                             double valor = Convert.ToDouble(dados_linha[1], System.Globalization.CultureInfo.InvariantCulture) ;
                             double quantidade = Convert.ToDouble(dados_linha[2], System.Globalization.CultureInfo.InvariantCulture) ;
 
                             double valor_total = valor * quantidade;
+                            //MOSTRANDO NA TELA A PRÉVIA
                             Console.WriteLine($"{nome} - {valor_total.ToString("f2",CultureInfo.InvariantCulture)}");
 
                             //ESCREVER NO ARQUIVO NOVO
                             //NOME,VALOR TOTAL
-                            //using (StreamWriter sw = File.AppendText(targetPath))
-                            //{
-                            //
-                            //}
+                            using (StreamWriter sw = File.AppendText(targetPath))
+                            {
+                               sw.WriteLine($"{nome},{valor_total.ToString("f2",CultureInfo.InvariantCulture)}");
+                            }
                         }
 
                     }
