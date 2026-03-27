27458d6a117417b5ede97ed4eff2290e323dc02e
lucianorb19 <52081214+lucianorb19@users.noreply.github.com>
2026-03-27 16:53:46 -0300

    Alura - Carreira cSharp

---
+++ Alura - carreira/1VariaveisETipos/1VariaveisETipos/Program.cs
@@ -0,0 +1,156 @@
+﻿using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+//--------------------------------------------------------------------------------------------
+/*
+int anoNascimento = 1997;
+int anoAtual = DateTime.Today.Year;
+int idade = anoAtual-anoNascimento;
+
+Console.WriteLine($"Ano nascimento: {anoNascimento}\nAno atual: {anoAtual}\nIdade:{idade}");
+
+//--------------------------------------------------------------------------------------------
+float valorRecebido;
+string opcao;
+bool doacaoAnonima;
+string tipoConta;
+
+
+Console.Write("Valor recebido: R$");
+valorRecebido = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.Write("Doação anônima? [S/N]: ");
+opcao =  Console.ReadLine().ToUpper();
+if(opcao == "S")
+{
+    doacaoAnonima=true;
+}
+else
+{
+    doacaoAnonima = false;
+}
+
+Console.Write("Tipo de conta [Corrente / Poupança]: ");
+opcao = Console.ReadLine().ToUpper();
+if (opcao == "CORRENTE")
+{
+    tipoConta = "C";
+}
+else if(opcao == "POUPANÇA")
+{
+    tipoConta = "P";
+}
+else
+{
+    tipoConta = "Indefinido";
+}
+
+    Console.WriteLine($"\nValor recebido: R${valorRecebido}\nDoação anônima: {doacaoAnonima}\nTipo de conta: {tipoConta}");
+
+//--------------------------------------------------------------------------------------------
+double distanciaEmMilhas;
+double distanciaEmQuilometros;
+double medidaConversao = 1.60934;
+
+Console.Write("Distância em milhas: ");
+distanciaEmMilhas = double.Parse(Console.ReadLine());
+distanciaEmQuilometros = (distanciaEmMilhas * medidaConversao);
+
+Console.WriteLine($"{distanciaEmMilhas} milhas equivalem a {distanciaEmQuilometros}km");
+
+//--------------------------------------------------------------------------------------------
+int valorMinutos;
+int horas;
+int minutos;
+
+Console.Write("Tempo decorrido em minutos: ");
+valorMinutos = int.Parse(Console.ReadLine());
+
+horas = (valorMinutos/60);
+minutos = (valorMinutos%60);
+
+Console.WriteLine($"Em horas\nHoras: {horas}\n+{minutos} minutos");
+
+//--------------------------------------------------------------------------------------------
+float largura;
+float comprimento;
+float areaRetangulo;
+
+Console.Write("Largura [m]: ");
+largura = float.Parse(Console.ReadLine());
+Console.Write("Comprimento [m]: ");
+comprimento = float.Parse(Console.ReadLine());
+areaRetangulo = largura * comprimento;
+
+Console.WriteLine($"A área do terreno é: {areaRetangulo} metros quadrados");
+
+//--------------------------------------------------------------------------------------------
+float nota1 = 7.2f;
+float nota2 = 8.3f;
+float nota3 = 9.1f;
+double mediaAritmeticaNotas = (nota1+ nota2 + nota3)/3;
+Console.WriteLine($"A média das notas é: {mediaAritmeticaNotas}");
+
+//--------------------------------------------------------------------------------------------
+decimal pesoContainers;
+int quantidadeVeiculosNecessarios;
+
+Console.Write("Peso dos containers em toneladas: ");
+pesoContainers = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+quantidadeVeiculosNecessarios = (int)pesoContainers;
+
+Console.WriteLine($"Veículos necessário: {quantidadeVeiculosNecessarios}");
+
+//--------------------------------------------------------------------------------------------
+byte vidasIniciais;
+byte vidasFinais;
+string opcao;
+
+Console.Write("Número inicial de vidas: ");
+vidasIniciais = byte.Parse(Console.ReadLine());
+vidasFinais = vidasIniciais;
+
+while (vidasFinais > 0)
+{
+    Console.Write("Perder vida [S/N]? ");
+    opcao = Console.ReadLine().ToUpper();
+    if(opcao == "S" || opcao == "SIM")
+    {
+        vidasFinais--;
+    }
+
+    Console.WriteLine($"Vidas: {vidasFinais}");
+}
+
+Console.WriteLine("Fim");
+//--------------------------------------------------------------------------------------------
+decimal salarioInicial;
+decimal percentualAumento;
+decimal valorAumento;
+decimal salarioIncrementado;
+
+Console.Write("Salário atual: R$ ");
+salarioInicial = decimal.Parse(Console.ReadLine());
+Console.Write("Percentual de aumento [%]: ");
+percentualAumento = decimal.Parse(Console.ReadLine());
+valorAumento = salarioInicial * (percentualAumento / 100);
+salarioIncrementado = salarioInicial + valorAumento;
+
+Console.WriteLine($"Salário inicial: R${salarioInicial.ToString("f2")}\n" +
+                  $"Percentual de aumento: {percentualAumento}% => R${valorAumento.ToString("f2")}\n" +
+                  $"Salário incrementado: R${salarioIncrementado.ToString("f2")}");
+*/
+//--------------------------------------------------------------------------------------------
+double pi = 3.14159;
+double raioCirculo;
+double areaCirculo;
+double circunferenciaCirculo;
+
+
+Console.Write("Raio da circunferência: ");
+raioCirculo = double.Parse(Console.ReadLine());
+areaCirculo = pi * raioCirculo * raioCirculo;
+circunferenciaCirculo = 2 * pi * raioCirculo;
+
+Console.WriteLine($"Raio: {raioCirculo}\nÁrea: {areaCirculo}\nCircunferência: {circunferenciaCirculo}");
+
+
---
+++ Alura - carreira/2Condicionais/2Condicionais/Program.cs
@@ -0,0 +1,273 @@
+﻿using System.Globalization;
+
+/*
+//----------------------------------------------------------------------------------------
+float saldo;
+
+Console.Write("Digite o saldo: ");
+saldo = float.Parse(Console.ReadLine());
+
+if(saldo == 0)
+{
+    Console.WriteLine("Saldo 0");
+}else if(saldo > 0)
+{
+    Console.WriteLine("Saldo positivo.");
+}
+else
+{
+    Console.WriteLine("Saldo negativo.");
+}
+
+//----------------------------------------------------------------------------------------
+int codigoProduto;
+
+Console.Write("Digite o código do produto \n1-Perecível\n2-Não perecível\n --> ");
+codigoProduto = int.Parse(Console.ReadLine());
+if(codigoProduto != 1 && codigoProduto != 2)
+{
+    Console.WriteLine("Código inválido!");
+}
+else
+{
+    if(codigoProduto == 1)
+    {
+        Console.WriteLine("Produto perecível!");
+    }
+    else //2
+    {
+        Console.WriteLine("Produto não perecível!");
+    }
+}
+
+//----------------------------------------------------------------------------------------
+
+
+float nota;
+string classificacaoNota;
+
+Console.Write("Informe a nota final do aluno: ");
+nota = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+if(nota >= 9)
+{
+    classificacaoNota = "A";
+}
+else if(nota >= 7 && nota < 9)
+{
+    classificacaoNota = "B";
+}
+else if(nota >= 5 && nota < 7)
+{
+    classificacaoNota = "C";
+}
+else// <5
+{
+    classificacaoNota = "D";
+}
+
+Console.WriteLine($"O aluno recebeu a nota {classificacaoNota}");
+
+//----------------------------------------------------------------------------------------
+string senha;
+int nivelAcesso;
+
+Console.Write("Nivel de acesso: ");
+nivelAcesso = int.Parse(Console.ReadLine());
+Console.Write("Senha: ");
+senha = Console.ReadLine();
+
+if (senha != "42" || nivelAcesso < 5)
+{
+    Console.WriteLine("Acesso negado!");
+    
+}
+else
+{
+    Console.WriteLine("Acesso liberado!");
+}
+
+//----------------------------------------------------------------------------------------
+int idade;
+string faixaEtaria = "";
+
+Console.Write("Idade: ");
+idade = int.Parse(Console.ReadLine());
+
+if(idade > 60)
+{
+    faixaEtaria = "Idoso";
+}
+else if(idade > 18 && idade <= 59)
+{
+    faixaEtaria = "Adulto";
+
+}
+else if(idade > 13 && idade <= 18)
+{
+    faixaEtaria = "Adolescente";
+
+}
+else if (idade > 0 && idade <= 13)
+{
+    faixaEtaria = "Infantil";
+
+}
+
+Console.WriteLine($"Classificação: {faixaEtaria}");
+
+//----------------------------------------------------------------------------------------
+float num1;
+float num2;
+string opcao;
+float resultado=0;
+
+Console.Write("Primeiro número: ");
+num1 = float.Parse(Console.ReadLine());
+Console.Write("Segundo número: ");
+num2 = float.Parse(Console.ReadLine());
+Console.Write("Operação (+, -, *, /)\n--> ");
+opcao = Console.ReadLine();
+
+switch (opcao)
+{
+    case "+":
+        resultado = num1 + num2;
+        break;
+    case "-":
+        resultado = num1 - num2;
+        break;
+    case "*":
+        resultado = num1 * num2;
+        break;
+    case "/":
+        resultado = num1 / num2;
+        break;
+}
+
+Console.WriteLine($"Resultado: {resultado}");
+
+//----------------------------------------------------------------------------------------
+int momentoDia;
+string nome = "";
+string mensagem = "";
+
+Console.Write("Qual o seu nome: ");
+nome = Console.ReadLine();
+Console.Write("Que momento do dia é agora?\n1-Manhã\n2-Tarde\n3-Noite\n--> ");
+momentoDia = int.Parse(Console.ReadLine());
+
+
+switch (momentoDia){
+    case 1:
+        mensagem = $"Boa manhã, {nome}!";
+        break;
+    case 2:
+        mensagem = $"Boa tarde, {nome}!";
+        break;
+    case 3:
+        mensagem = $"Boa noite, {nome}!";
+        break;
+    default:
+        mensagem = "Bom dia!";
+        break;
+}
+
+Console.WriteLine(mensagem);
+
+//----------------------------------------------------------------------------------------
+string recompensa = "";
+string mensagem = "";
+
+Console.Write("Código de recompensa\n--DOBRAR\n--CURAR\n--OURO\n--ESPECIAL\n--> ");
+recompensa = Console.ReadLine().ToUpper();
+
+mensagem = recompensa switch
+{
+    "DOBRAR" => "Ganhar 2x EXP por 1 hora desbloqueado!",
+    "CURAR" => "Poção de cura desbloqueada!",
+    "OURO" => "1000 moedas desbloqueadas!",
+    "ESPECIAL" => "Item lendário desbloqueado!",
+    _ => "Código inválido"
+};
+
+Console.WriteLine(mensagem);
+
+//----------------------------------------------------------------------------------------
+int codigoLivro;
+
+Console.Write("Digite o código do livro: ");
+codigoLivro = int.Parse(Console.ReadLine());
+
+switch (codigoLivro/100)
+{
+    case 1:
+        Console.WriteLine("Ficção Científica");
+        break;
+    case 2:
+        Console.WriteLine("Literatura Clássica");
+        break;
+    case 3:
+        Console.WriteLine("Fantasia");
+        break;
+    case 4:
+        Console.WriteLine("Romance");
+        break;
+    case 5:
+        Console.WriteLine("Suspense/Mistério");
+        break;
+    case 6:
+        Console.WriteLine("Não ficção");
+        break;
+    case 7:
+        Console.WriteLine("Biografias/Memórias");
+        break;
+    case 8:
+        Console.WriteLine("Distopia");
+        break;
+    case 9:
+        Console.WriteLine("Infantojuvenil");
+        break;
+    default:
+        Console.WriteLine("Código inexistente");
+        break;
+}
+
+//----------------------------------------------------------------------------------------
+
+string nome = "";
+int opcao;
+
+Console.WriteLine("Sistema de Autenticação\n------------------------------");
+Console.Write("Nome de usuarío: ");
+nome = Console.ReadLine().ToUpper();
+
+if(nome == "ADMIN")
+{
+    Console.WriteLine("Usuário admin logado!");
+}
+else
+{
+    Console.WriteLine("Usuário não cadastrado!\n" +
+                      "[1] - Cadastrar novo usuário\n" +
+                      "[2] - Acessar como convidado\n" +
+                      "[3] - Sair");
+    opcao = int.Parse(Console.ReadLine());
+
+    switch (opcao)
+    {
+        case 1:
+            Console.WriteLine($"Novo usuário {nome} cadastrado com sucesso.");
+            break;
+        case 2:
+            Console.WriteLine($"{nome} logado como convidado.");
+            break;
+        case 3:
+            Console.WriteLine("Logout efetuado.");
+            break;
+        default:
+            Console.WriteLine("Opção inválida!");
+            break;
+    }
+}
+*/
