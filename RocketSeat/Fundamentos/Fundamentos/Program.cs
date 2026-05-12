fed621bd28729442a5790bf312bb164c95671201
lucianorb19 <52081214+lucianorb19@users.noreply.github.com>
2026-05-12 16:41:26 -0300

    RS - Fundamentos e anotações

---
+++ RocketSeat/Fundamentos/Fundamentos/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+//----------------------------------------------------------------
+Console.Write("Qual seu nome?\n-> ");
+string nome = Console.ReadLine();
+Console.WriteLine($"Olá, {nome}! Seja bem-vindo!");
+
+//----------------------------------------------------------------
+Console.Write("Qual seu nome?\n-> ");
+string nome = Console.ReadLine();
+Console.Write("Qual seu sobrenome?\n-> ");
+string sobrenome = Console.ReadLine();
+Console.WriteLine($"Nome completo: {nome} {sobrenome}");
+
+//----------------------------------------------------------------
+Console.Write("Primeiro número\n--> ");
+double primeiroNumero = double.Parse(Console.ReadLine());
+Console.Write("Primeiro número\n--> ");
+double segundoNumero = double.Parse(Console.ReadLine());
+
+Console.Write($"Soma: {primeiroNumero+segundoNumero}\n" +
+                  $"Subtração: {primeiroNumero - segundoNumero}\n" +
+                  $"Multiplicação: {primeiroNumero * segundoNumero}\n");
+if(segundoNumero == 0)
+{
+    Console.WriteLine($"Divisão: Impossível realizar divisão por 0.");
+}
+else
+{
+    Console.WriteLine($"Divisão: {primeiroNumero / segundoNumero}");
+}
+Console.WriteLine($"Media: {(primeiroNumero+segundoNumero)/2}");
+
+//----------------------------------------------------------------
+Console.Write("Digite uma palavra ou frase qualquer:\n--> ");
+string fraseOuPalavra = Console.ReadLine().Trim();
+string frasePalavraSemEspacos = fraseOuPalavra.Replace(" ", "");
+int tamanho = frasePalavraSemEspacos.Length;
+Console.WriteLine($"\nQuantidade de caractéres na frase/palavra (desconsiderando espaços vazios):" +
+                  $" {tamanho}");
+*/
+//----------------------------------------------------------------
+
+
+//----------------------------------------------------------------
+//----------------------------------------------------------------
+//----------------------------------------------------------------
+//----------------------------------------------------------------
+//----------------------------------------------------------------
