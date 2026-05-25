19d0ac780c5d59a574cf8e9d818cb5154499ebde
lucianorb19 <52081214+lucianorb19@users.noreply.github.com>
2026-05-25 16:30:22 -0300

    Projeto de API - CashFlow - inicio

---
+++ RocketSeat/CashFlow/src/CashFlow.API/Controllers/ExpensesController.cs
@@ -0,0 +1,32 @@
+﻿using CashFlow.Application.UseCases.Expenses;
+using CashFlow.Communication.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashFlow.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ExpensesController : ControllerBase
+{
+    [HttpPost]
+    //[ProducesResponseType()]
+    public IActionResult Register([FromBody] RequestRegisterExpenseJson request)
+    {
+        try
+        {
+            var response = new RegisterExpenseUseCase().Execute(request);
+            return Created(string.Empty, response);
+        }
+        catch (ArgumentException ex)
+        {
+            return BadRequest(ex.Message);
+        }
+        catch
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError,"Erro desconhecido");
+        }
+
+
+        
+    }
+}
---
+++ RocketSeat/CashFlow/src/CashFlow.Application/UseCases/Expenses/RegisterExpenseUseCase.cs
@@ -0,0 +1,33 @@
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+
+namespace CashFlow.Application.UseCases.Expenses;
+public class RegisterExpenseUseCase
+{
+    public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
+    {
+        Validate(request);
+        return new ResponseRegisteredExpenseJson();
+    }
+
+    private void Validate(RequestRegisterExpenseJson request)
+    {
+        //TITULO
+        var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
+        if (titleIsEmpty) throw new ArgumentException("O título precisa ser preenchido.");
+
+        //VALOR
+        if (request.Amount <= 0) throw new ArgumentException("O valor da despesa precisa ser maior que zero.");
+
+        //DATA
+        var comparacaoDatas = DateTime.Compare(request.Date,DateTime.UtcNow);
+        if (comparacaoDatas > 0) throw new ArgumentException("Data da despesa não pode ser futura.");
+
+        //TIPO DE PAGAMENTO
+        var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
+        if (paymentTypeIsValid == false) throw new ArgumentException("Tipo de despesa não é válido.");
+    }
+
+    
+}
