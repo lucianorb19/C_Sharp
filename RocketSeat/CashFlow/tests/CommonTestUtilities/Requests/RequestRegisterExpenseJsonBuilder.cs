8d59b6af98d833923ee5e9167eb1cbfc2e9c7fba
lucianorb19 <52081214+lucianorb19@users.noreply.github.com>
2026-05-28 15:14:09 -0300

    Projeto de API - CashFlow - Testes Unitário
    Testes unitários com xunit para a validação dos campos de registro da despesa.

---
+++ RocketSeat/CashFlow/tests/CommonTestUtilities/Requests/RequestRegisterExpenseJsonBuilder.cs
@@ -0,0 +1,33 @@
+﻿using Bogus;
+using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+
+namespace CommonTestUtilities.Requests;
+public class RequestRegisterExpenseJsonBuilder
+{
+    public static RequestRegisterExpenseJson Build()
+    {
+        //1ª MANEIRA DE USAR FAKER
+        /*
+        var faker = new Faker();
+        var request = new RequestRegisterExpenseJson
+        {
+            Title = faker.Commerce.ProductName(),
+            Description = faker.Commerce.ProductDescription(),
+            Date = faker.Date.Past(),
+            PaymentType = faker.PickRandom<PaymentType>(),
+            Amount = faker.Random.Decimal(),
+
+        };
+        return request;
+        */
+
+        //2ª MANEIRA DE USAR FAKER
+        return new Faker<RequestRegisterExpenseJson>()
+            .RuleFor(r => r.Title, faker => faker.Commerce.ProductName())
+            .RuleFor(r => r.Description, faker => faker.Commerce.ProductDescription())
+            .RuleFor(r => r.Date, faker => faker.Date.Past())
+            .RuleFor(r => r.PaymentType, faker => faker.PickRandom<PaymentType>())
+            .RuleFor(r => r.Amount, faker => faker.Random.Decimal());
+    }
+}
---
+++ RocketSeat/CashFlow/tests/Validators.Tests/Expenses/RegisterExpenseValidatorTests.cs
@@ -0,0 +1,117 @@
+﻿using CashFlow.Application.UseCases.Expenses;
+using CashFlow.Communication.Enums;
+using CashFlow.Exception;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+
+namespace Validators.Tests.Expenses;
+public class RegisterExpenseValidatorTests
+{
+    [Fact]
+    public void Sucess_ExpenseRegisterWithValidFieldsIsValid()
+    {
+        //ARRANGE
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        
+
+        //ACT
+        var result = validator.Validate(request);
+
+        //ASSERT TRADICIONAL
+        //Assert.True(result.IsValid);
+
+        //ASSERT COM FLUENTASSERTION
+        result.IsValid.Should().BeTrue();
+
+        //COM O FLUENTASSERTION PAGO A PARTIR DA VERSÃO 8.0
+        //UMA ALTERNATIVA É A BIBLIOTECA SHOULDLY
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("      ")]
+    [InlineData(null)]
+    public void ErrorEmptyOrWhiteSpaceOrNullTytle(string emptyTitle)
+    {
+        //ARRANGE
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.Title = emptyTitle;
+        
+        //ACT
+        var result = validator.Validate(request);
+
+        //ASSERT COM FLUENTASSERTION
+        result.IsValid.Should().BeFalse();//TEM QUE GERAR ERRO
+        //TEM QUE SER SOMENTE UM ERRO, COM A STRING ESPECÍFICA QUE GERAMOS EM RESOURCES
+        result.Errors.Should().ContainSingle()
+              .And.Contain(erro => erro.ErrorMessage.Equals(
+                     ResourceErrorMessages.TITLE_REQUIRED));
+    }
+
+    [Fact]
+    public void ErrorFutureExpenseDate()
+    {
+        //ARRANGE
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.Date = DateTime.UtcNow.AddDays(2);
+
+        //ACT
+        var result = validator.Validate(request);
+
+        //ASSERT COM FLUENTASSERTION
+        result.IsValid.Should().BeFalse();//TEM QUE GERAR ERRO
+        //TEM QUE SER SOMENTE UM ERRO, COM A STRING ESPECÍFICA QUE GERAMOS EM RESOURCES
+        result.Errors.Should().ContainSingle()
+              .And.Contain(erro => erro.ErrorMessage.Equals(
+                     ResourceErrorMessages.EXPENSE_DATE_NOT_FUTURE));
+    }
+
+    [Fact]
+    public void ErrorPaymentTypeNotAValidEnum()
+    {
+        //ARRANGE
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.PaymentType = (PaymentType)8;
+
+        //ACT
+        var result = validator.Validate(request);
+
+        //ASSERT COM FLUENTASSERTION
+        result.IsValid.Should().BeFalse();//TEM QUE GERAR ERRO
+        //TEM QUE SER SOMENTE UM ERRO, COM A STRING ESPECÍFICA QUE GERAMOS EM RESOURCES
+        result.Errors.Should().ContainSingle()
+              .And.Contain(erro => erro.ErrorMessage.Equals(
+                     ResourceErrorMessages.EXPENSE_TYPE_NOT_VALID));
+    }
+
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    [InlineData(-40)]
+    public void ErrorAmountLessThanOrEqualToZero(decimal testAmount)
+    {
+        //ARRANGE
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.Amount = testAmount;
+
+        //ACT
+        var result = validator.Validate(request);
+
+        //ASSERT COM FLUENTASSERTION
+        result.IsValid.Should().BeFalse();//TEM QUE GERAR ERRO
+        //TEM QUE SER SOMENTE UM ERRO, COM A STRING ESPECÍFICA QUE GERAMOS EM RESOURCES
+        result.Errors.Should().ContainSingle()
+              .And.Contain(erro => erro.ErrorMessage.Equals(
+                     ResourceErrorMessages.AMOUNT_GREATER_THAN_ZERO));
+    }
+
+
+
+
+}
