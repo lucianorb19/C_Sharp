2b169246cdd9c13ff0b47f78d165e8e1c86ab48b
lucianorb19 <52081214+lucianorb19@users.noreply.github.com>
2026-06-18 17:03:59 -0300

    Endpoint para Geração de PDF - início

---
+++ RocketSeat/CashFlow/src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
@@ -0,0 +1,125 @@
+﻿
+using CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts;
+using CashFlow.Domain.Reports;
+using CashFlow.Domain.Repositories.Expenses;
+using MigraDoc.DocumentObjectModel;
+using MigraDoc.Rendering;
+using PdfSharp.Fonts;
+using System.Reflection;
+
+namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf;
+internal class GenerateExpensesReportPdfUseCase : IGenerateExpensesReportPdfUseCase
+{
+    private const string CURRENCY_SYMBOL = "R$";
+    private readonly IExpensesReadOnlyRepository _repository;
+
+    public GenerateExpensesReportPdfUseCase(IExpensesReadOnlyRepository repository)
+    {
+        _repository = repository;
+        GlobalFontSettings.FontResolver = new ExpensesReportFontResolver();
+    }
+
+    public async Task<byte[]> Execute(DateOnly month)
+    {
+        var expenses = await _repository.FilterByMonth(month);
+        var totalExpenses = expenses.Sum(expense => expense.Amount);
+        if (expenses.Count == 0) return [];
+
+        var document = CreateDocument(month);
+        var page = CreatePage(document);
+
+        CreateHeaderWithProfilePhotoAndName(page);
+        
+        CreateTotalExpensesSection(page, month, totalExpenses);
+
+        return RenderDocument(document);
+    }
+
+    //FUNÇÕES AUXILIARES
+    private Document CreateDocument(DateOnly month)
+    {
+        var document = new Document();
+        document.Info.Title = $"{ResourceReportGenerationMessages.EXPENSES_FOR} {month.ToString("Y")}";
+        document.Info.Author = "Luciano Rodrigues Batista";
+
+        //FONTE PADRÃO
+        var styles = document.Styles["Normal"];
+        styles!.Font.Name = FontHelper.RALEWAY_REGULAR;
+
+        return document;
+    }
+
+    private Section CreatePage(Document document)
+    {
+        var section = document.AddSection();
+        section.PageSetup = document.DefaultPageSetup.Clone();
+
+        section.PageSetup.PageFormat = PageFormat.A4;
+        section.PageSetup.LeftMargin = 40;
+        section.PageSetup.RightMargin = 40;
+        section.PageSetup.TopMargin = 80;
+        section.PageSetup.BottomMargin = 80;
+
+        return section;
+    }
+
+    private void CreateHeaderWithProfilePhotoAndName(Section page)
+    {
+        //FOTO E MENSAGEM BOAS VINDAS
+        var table = page.AddTable();
+        table.AddColumn();
+        table.AddColumn("300");
+
+        var row = table.AddRow();
+        var assembly = Assembly.GetExecutingAssembly();
+        var directoryName = Path.GetDirectoryName(assembly.Location);
+        var pathFile = Path.Combine(directoryName!, "Logo", "foto_perfilPequena.png");
+
+        row.Cells[0].AddImage(pathFile);
+        row.Cells[1].AddParagraph($"{ResourceReportGenerationMessages.WELCOME_MESSAGE} Luciano!");
+        row.Cells[1].Format.Font = new Font { Name = FontHelper.RALEWAY_BLACK, Size = 16 };
+        row.Cells[1].VerticalAlignment = MigraDoc.DocumentObjectModel.Tables.VerticalAlignment.Center;
+    }
+
+    private void CreateTotalExpensesSection(Section page, DateOnly month, decimal totalExpenses)
+    {
+        //TÍTULO DO PDF
+        //FUNÇÃO QUE PREENCHE O {} DO ARQUIVO ResourceReportGenerationMessages.resx
+        //TOTAL GASTO EM JUNHO 2026
+        var title = string.Format(ResourceReportGenerationMessages.TOTAL_SPENT_IN, month.ToString("Y"));
+
+        var paragraph = page.AddParagraph();
+        paragraph.Format.SpaceAfter = 40;
+        paragraph.Format.SpaceBefore = 40;
+
+        paragraph.AddFormattedText(title, new Font
+        {
+            Name = FontHelper.RALEWAY_REGULAR,
+            Size = 15
+        });
+        paragraph.AddLineBreak();
+
+        paragraph.AddFormattedText($"{totalExpenses} {CURRENCY_SYMBOL}",
+                                     new Font
+                                     {
+                                         Name = FontHelper.WORKSANS_BLACK,
+                                         Size = 50
+                                     });
+    }
+
+    private byte[] RenderDocument(Document document)
+    {
+        var renderer = new PdfDocumentRenderer
+        {
+            Document = document,
+        };
+        renderer.RenderDocument();
+
+        using var file = new MemoryStream();
+        renderer.PdfDocument.Save(file);
+
+        return file.ToArray();
+    }
+
+   
+}
